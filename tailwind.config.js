6372643cae6ba376a88e0b54fcb5e943434d7176
cAndrewc <andrewzou2004@gmail.com>
2024-08-15 22:29:49 -0400

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html','./src/**/*.{jsx,js}'],
+  darkMode: 'class',
+  theme: {
+
+    
+
+    extend: {
+      fontFamily: {
+        
+      },
+      colors: {
+        'sky': {
+        400: '#0091ff', 
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
